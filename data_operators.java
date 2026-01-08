89474a48c2d7f29d2617a714eeffe406a5f2ba09
Jaswanth Reddy <m.jaswanth0905@gmail.com>
2026-01-08 14:24:48 +0530

    Add files via upload

---
+++ data_operators.java
@@ -0,0 +1,32 @@
+public class data_operators{
+public static void main(String[] args){
+int a= 10;
+int b=5;
+int sum = a+b;
+int diff = a-b;
+int multi = a*b;
+int divi = a/b;
+int mod = a%b;
+int increment = a++;
+int decrement = b--;
+boolean less = a<b;
+boolean less_equal = a<=b;
+boolean greater = a>b;
+boolean greater_equal = a>=b;
+boolean equal = a==b;
+boolean not_equal = a!=b;
+System.out.println("Sum : "+sum);
+System.out.println("Difference : "+diff);
+System.out.println("Multiplication : "+multi);
+System.out.println("Division : "+divi);
+System.out.println("Moduluds : "+mod);
+System.out.println("Increment : "+increment );
+System.out.println("Decrement: "+decrement );
+System.out.println("a Lessthan b : "+less);
+System.out.println("a Lessthan or equal b : "+less_equal);
+System.out.println("a greaterthan b : "+greater);
+System.out.println("a greaterthan or equal b : "+greater_equal);
+System.out.println(" Is a and b are equal : "+equal);
+System.out.println(" Is a and b are equal : "+not_equal);
+}
+}
