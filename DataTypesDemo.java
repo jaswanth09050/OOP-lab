41a752901b8c735be9235ebc2745a85331cce316
Jaswanth Reddy <m.jaswanth0905@gmail.com>
2026-01-08 14:41:02 +0530

    Add files via upload

---
+++ DataTypesDemo.java
@@ -0,0 +1,20 @@
+public class DataTypesDemo {
+    public static void main(String[] args) {
+        byte b = 10;         
+        short s = 200;        
+        int i = 10000;        
+        long l = 100000000L;  
+        float f = 12.5f;      
+        double d = 123.456;							
+        char c = 'A';        
+        boolean flag = true;  
+        System.out.println("Byte value: " + b);
+        System.out.println("Short value: " + s);
+        System.out.println("Int value: " + i);
+        System.out.println("Long value: " + l);																													
+        System.out.println("Float value: " + f);
+        System.out.println("Double value: " + d);
+        System.out.println("Character value: " + c);
+        System.out.println("Boolean value: " + flag);
+    }
+}
